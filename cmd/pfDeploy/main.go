0cd669b6185f61de782e71d1436e5bdcf353c002
erodrigufer <erodrigufer@gmail.com>
2022-07-30 01:07:32 +0200

    add package description

--- cmd/pfDeploy/main.go
+++ cmd/pfDeploy/main.go
@@ -1,3 +1,5 @@
+// pfDeploy, configures pf on a system to sane defaults, checks a given rule set
+// syntax, enables pf with the given rule set and finally reboots the system.
 package main
 
 func main() {

80ab2c5fb2d6309333f17e757cae8578809a7d6f
erodrigufer <erodrigufer@gmail.com>
2022-08-10 16:17:36 +0200

    refactor CLI methods to own file

--- cmd/pfDeploy/helpers.go
+++ cmd/pfDeploy/helpers.go
@@ -7,8 +7,6 @@ import (
 
 	"github.com/erodrigufer/pfDeploy/internal/sysutils"
 	"github.com/erodrigufer/pfDeploy/pfSetup"
-
-	"github.com/urfave/cli/v2"
 )
 
 // setupApplication, configures all needed general parameters for the
@@ -23,77 +21,6 @@ func (app *application) setupApplication() {
 	// Create an ERROR messages logger, addiotionally use the Lshortfile flag to
 	// display the file's name and line number for the error.
 	app.errorLog = log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-}
-
-// runTUI, run the TUI (Terminal User Interface), handled by the CLI package.
-func (app *application) runTUI() {
-	app.setupCLI()
-
-	if err := app.tui.Run(os.Args); err != nil {
-		app.errorLog.Fatal(err)
-	}
-}
-
-// setupCLI, configure and initialize all commands, flags and options of
-// the TUI.
-func (app *application) setupCLI() {
-	app.tui = &cli.App{
-		Name:  "pfDeploy",
-		Usage: "Automatically setup pf in your new deployment.",
-		// This options enables short flag abbreviations to be merged into a
-		// single flag with a '-' prefix.
-		UseShortOptionHandling: true,
-		Commands: []*cli.Command{
-			&cli.Command{
-				Name:  "deploy",
-				Usage: "Setup pf and pflog at boot and deploy a new pf rules file.",
-				// Flags for deploy command.
-				Flags: []cli.Flag{
-					&cli.StringFlag{
-						Name:     "file",
-						Aliases:  []string{"f"},
-						Required: true,
-						Usage:    "`PATH` to the file used as the new pf rule set.",
-					},
-					&cli.BoolFlag{
-						Name:    "reboot",
-						Aliases: []string{"r"},
-						Value:   true,
-						Usage:   "Reboot host after deploying new pf configuration.",
-					},
-				},
-				Action: func(cCtx *cli.Context) error {
-					if err := app.deploy(cCtx.String("file"), cCtx.Bool("reboot")); err != nil {
-						err = fmt.Errorf("error while executing 'deploy' command: %w", err)
-						return cli.Exit(err, 1)
-					}
-					return nil
-				},
-			},
-			&cli.Command{
-				Name:  "check",
-				Usage: "Check the syntactical validity of a pf rules file.",
-				Flags: []cli.Flag{
-					&cli.StringFlag{
-						Name:     "file",
-						Aliases:  []string{"f"},
-						Required: true,
-						Usage:    "`PATH` to the file used as the pf rule set.",
-					},
-				},
-				Action: func(cCtx *cli.Context) error {
-					// Read input for -file flag.
-					filePath := cCtx.String("file")
-					if err := app.checkRuleSet(filePath); err != nil {
-						err = fmt.Errorf("error while executing 'check' command: %w", err)
-						return cli.Exit(err, 1)
-					}
-					return nil
-				},
-			},
-		},
-	}
-
 }
 
 // deploy, runs the main application, by encapsulating the main application's

bbe3f745213d2d8ea4f5d3ca59c948ba0275fea9
erodrigufer <erodrigufer@gmail.com>
2022-09-13 11:10:02 +0200

    update main package description

--- cmd/pfDeploy/main.go
+++ cmd/pfDeploy/main.go
@@ -1,3 +1,6 @@
+// pfDeploy is a command-line tool capable of automatically configuring pf
+// and pflog in a FreeBSD host machine. It is especially useful to quickly
+// configure a newly spawned FreeBSD VM.
 // pfDeploy, configures pf on a system to sane defaults, checks a given rule set
 // syntax, enables pf with the given rule set and finally reboots the system.
 package main
